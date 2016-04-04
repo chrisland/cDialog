261e283017be29595eb02b8b95772bbab153f371
Christian Marienfeld <post@chrisland.de>
2016-04-04 16:37:24 +0200

    version 0.0.2 // notif / error function

--- src/cDialog.js
+++ src/cDialog.js
@@ -2,7 +2,7 @@
 * Easy JS Dialog system
 *
 * @class cDialog
-* @version 0.0.1
+* @version 0.0.2
 * @license MIT
 *
 * @author Christian Marienfeld post@chrisand.de
@@ -31,7 +31,7 @@
 *
 *	myDialog.hide();
 *
-*	myDialog.destroy(); 
+*	myDialog.destroy();
 *
 *
 * @return {Object} cDialog Object
@@ -63,7 +63,7 @@ function cDialog(param) {
 	if (this._opt.container) {
 		this._page = document.getElementById(this._opt.container);
 	}
-	
+
 	if (!this._page) {
 		throw new Error("missing main container #"+this._opt.container);
 		this._page = document.body;
@@ -71,29 +71,60 @@ function cDialog(param) {
 
 
 	// APPEND
-	
+
 	this._wrap = document.createElement('div');
 	this._wrap.id = 'cDialog-'+(Math.floor(Math.random()*11)+2)*(Math.floor(Math.random()*11)+2);
+	this._wrap.className = 'cDialog'
 	this._wrap.style.display = 'none';
-	
+
 	this._node = {};
-	
+
 	var that = this;
-	
-	if (this._opt.type == 'input') {
-		
-		this._node.info = document.createElement('div');
-		this._node.info.innerHTML = 'Hier eingeben....';
-		this._wrap.appendChild(this._node.info);
-		
+	if (this._opt.type == 'notif') {
+
+		if (!this._opt.msg) {
+			return false;
+		}
+
+		this._node.msg = document.createElement('div');
+		this._node.msg.className = 'cDialog-msg';
+		this._node.msg.innerHTML = this._opt.msg;
+		this._wrap.appendChild(this._node.msg);
+
+
+	} else if (this._opt.type == 'error') {
+
+		if (!this._opt.msg) {
+			return false;
+		}
+
+		this._node.msg = document.createElement('div');
+		this._node.msg.className = 'cDialog-error';
+		this._node.msg.innerHTML = this._opt.msg;
+		this._wrap.appendChild(this._node.msg);
+
+
+
+	} else if (this._opt.type == 'input') {
+
+		if (this._opt.info) {
+			this._node.info = document.createElement('div');
+			this._node.info.className = 'cDialog-info';
+			this._node.info.innerHTML = this._opt.info;
+			this._wrap.appendChild(this._node.info);
+		}
+
+
 		this._node.field = document.createElement('input');
 		this._node.field.type = 'text';
+		this._node.field.className = 'cDialog-input';
 		this._wrap.appendChild(this._node.field);
-		
+
 		if (this._opt.confirm && this._opt.confirm.text && this._opt.confirm.handler) {
-			
+
 			this._node.confirm = document.createElement('button');
 			this._node.confirm.innerHTML = this._opt.confirm.text;
+			this._node.confirm.className = 'cDialog-confirm';
 			this._node.confirm.onclick = function (e) {
 				that._opt.confirm.handler(that._node.field.value, e);
 				if (that._opt.destroy == true) {
@@ -103,13 +134,14 @@ function cDialog(param) {
 				}
 			};
 			this._wrap.appendChild(this._node.confirm);
-			
+
 		}
-		
+
 		if (this._opt.abort && this._opt.abort.text && this._opt.abort.handler) {
-				
+
 			this._node.abort = document.createElement('button');
 			this._node.abort.innerHTML = this._opt.abort.text;
+			this._node.abort.className = 'cDialog-abort';
 			this._node.abort.onclick = function (e) {
 				that._opt.abort.handler(e);
 				if (that._opt.destroy == true) {
@@ -120,14 +152,24 @@ function cDialog(param) {
 			};
 			this._wrap.appendChild(this._node.abort);
 		}
-		
+
 	}
-	
+
 	this._page.appendChild(this._wrap);
 
 
 	if (this._opt.autoOpen) {
 		this.show();
+	}
+
+	if (this._opt.autoHide) {
+		if (!isNaN(this._opt.autoHide)) {
+			this._opt.autoHide = 2000;
+		}
+		setTimeout(function () {
+			that.destroy();
+		}, this._opt.autoHide)
+
 	}
 
 	return this;
@@ -136,31 +178,29 @@ function cDialog(param) {
 
 
 cDialog.prototype.show = function () {
-	
+
 	this._wrap.style.display = 'block';
-	
+
 	return this;
-	
+
 };
 
 
 
 cDialog.prototype.hide = function () {
-	
+
 	this._wrap.style.display = 'none';
-	
+
 	return this;
-	
+
 };
 
 
 cDialog.prototype.destroy = function () {
-	
+
 	document.body.removeChild(this._wrap);
-	
+
 
 	return this;
-	
+
 };
-
-
